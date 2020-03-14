ba0e52295302d2a0a7d1e7432f190c6621745bf7
Nick <nstebner2552@gmail.com>
2020-03-13 21:52:41 -0700

    Added the review page html and basic component pieces

--- client-angular/src/app/app.module.ts
+++ client-angular/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { TaskDisplayComponent } from "./task-display/task-display.component";
+import { ReviewsComponent } from './reviews/reviews.component';
 
 @NgModule({
-  declarations: [AppComponent, TaskDisplayComponent],
+  declarations: [AppComponent, TaskDisplayComponent, ReviewsComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
