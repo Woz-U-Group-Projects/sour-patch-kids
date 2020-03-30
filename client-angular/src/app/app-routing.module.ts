1299093e3c23b847a39d0d7b049a57319faaec39
Grand <glucersda@gmail.com>
2020-03-29 23:00:05 -0400

    added home page plus routing and navigation

--- client-angular/src/app/app-routing.module.ts
+++ client-angular/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { MenuComponent } from './menu/menu.component';
+import { DetailComponent } from './detail/detail.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: "", pathMatch: "full", component: HomeComponent},
+  {path: "about", component: AboutComponent},
+  {path: "contact", component: ContactComponent},
+  {path: "menu", component: MenuComponent},
+  {path: "detail/:id", component: DetailComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client-angular/src/app/app.module.ts
+++ client-angular/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { TaskDisplayComponent } from "./task-display/task-display.component";
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { NavComponent } from './nav/nav.component';
+import { DetailComponent } from './detail/detail.component';
+
 
 @NgModule({
-  declarations: [AppComponent, TaskDisplayComponent],
+  declarations: [AppComponent, HomeComponent, MenuComponent, AboutComponent, ContactComponent, NavComponent, DetailComponent, ],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
